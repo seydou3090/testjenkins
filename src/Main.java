1464bbeb0c5da2897dd7056536ec8aa7f8b6a0e0
Seydou TRAORE <Seydou.TRAORE@orangemali.com>
2024-08-03 17:51:57 +0000

    ddd2

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Bonjour SEYDOU");
+		System.out.print("Bonjour SEYDOU 222");
 
 	}
 
